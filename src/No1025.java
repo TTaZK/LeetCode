7aa8055c8d14ec394c48523efba46f18b83b1d1f
zk <1049165980@qq.com>
2019-04-23 22:50:53 +0800

    添加第300，1025题

---
+++ src/No1025.java
@@ -0,0 +1,12 @@
+import java.util.Vector;
+
+public class No1025 {
+    public boolean divisorGame(int N) {
+        if(N % 2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+}
