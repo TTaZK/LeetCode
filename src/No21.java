40dc7f1b1db91a67ca9ea572346ec1ca7fdaca47
zk <1049165980@qq.com>
2019-05-21 23:13:28 +0800

    添加第24，3题

---
+++ src/No21.java
@@ -0,0 +1,32 @@
+public class No21 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head, temp;
+        head = temp = new ListNode(0);
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                temp.next = l2;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else if (l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        return head.next;
+    }
+}
---
+++ src/No3.java
@@ -0,0 +1,31 @@
+public class No3 {
+    // 该题目主要使用回溯，想要确定第i位置的长度，那么需要将第i个字符与前f[i-1]个字符进行比较
+    // 如果与倒数第一个字符相等，则f[i]=1；如果与倒数第x字符相等，则f[i] = (x -1) + 1 = x
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        if (n <= 0) {
+            return 0;
+        }
+        int[] f = new int[n];
+        f[0] = 1;
+        for (int i = 1; i < n; i++) {
+            f[i] = f[i - 1] + 1;
+            char c = s.charAt(i);
+            for (int j = 0; j < f[i - 1]; j++) {
+                if (s.charAt(i - j - 1) == c) {
+                    f[i] = j + 1;
+                    break;
+                }
+            }
+        }
+        int max = 1;
+        for (int i = 0; i < n; i++) {
+            max = max < f[i] ? f[i] : max;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabbcvbr"));
+    }
+}
