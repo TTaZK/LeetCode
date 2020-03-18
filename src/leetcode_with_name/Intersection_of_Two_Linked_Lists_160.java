b93b7d48064e9732b1ef876ad174338e163f7be0
张凯凯 <zhangkaikai@bytedance.com>
2020-03-18 20:44:07 +0800

    finish no 160

--- src/leetcode_with_name/Intersection_of_Two_Linked_Lists_160.java
+++ src/leetcode_with_name/Intersection_of_Two_Linked_Lists_160.java
@@ -1,5 +1,7 @@
 package leetcode_with_name;
 
+import java.util.Stack;
+
 public class Intersection_of_Two_Linked_Lists_160 {
     class ListNode {
         int val;
@@ -10,7 +12,34 @@ public class Intersection_of_Two_Linked_Lists_160 {
             next = null;
         }
     }
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
+    // 利用栈FILO的特点，从后向前遍历
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        Stack<ListNode> stackA = new Stack<>();
+        Stack<ListNode> stackB = new Stack<>();
+        // push link A into stack
+        while (headA != null) {
+            stackA.push(headA);
+            headA = headA.next;
+        }
+        // push link B into stack
+        while (headB != null) {
+            stackB.push(headB);
+            headB = headB.next;
+        }
+        // pop and compare
+        ListNode ans = null;
+        while (!stackA.isEmpty() && !stackB.isEmpty()) {
+            ListNode topA = stackA.pop();
+            ListNode topB = stackB.pop();
+            if (topB != topA) {
+                return ans;
+            }
+            ans = topA;
+        }
+        return ans;
     }
 }
