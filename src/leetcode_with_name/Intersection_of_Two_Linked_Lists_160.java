ef50e41a63f4c2690daad1c2c8e3950eef27b7ee
张凯凯 <zhangkaikai@bytedance.com>
2020-03-18 20:24:17 +0800

    add no 160

---
+++ src/leetcode_with_name/Intersection_of_Two_Linked_Lists_160.java
@@ -0,0 +1,16 @@
+package leetcode_with_name;
+
+public class Intersection_of_Two_Linked_Lists_160 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+    }
+}
