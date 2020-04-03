b2d8c2bc46da0fe1f2899472ea7d21e6a5cc437c
张凯凯 <zhangkaikai@bytedance.com>
2020-04-03 10:49:26 +0800

    add no.90

--- src/leetcode_with_name/Subsets_II_90.java
+++ src/leetcode_with_name/Subsets_II_90.java
@@ -8,6 +8,7 @@ import java.util.Set;
 public class Subsets_II_90 {
     // 利用回溯法求出所有的子集，在计算所有的子集的时候可以按照顺序插入
     // 然后对子集进行过滤
+    // ac
     public static List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         Set<List<Integer>> set = new HashSet<>();
@@ -20,10 +21,6 @@ public class Subsets_II_90 {
 
     public static void backTrack(int[] nums, int curLevel, List<Integer> cur, Set<List<Integer>> res) {
         if (curLevel >= nums.length) {
-//            for (int v : cur) {
-//                System.out.print(v + " ");
-//            }
-//            System.out.println();
             res.add(new ArrayList<>(cur));
         } else {
             int value = nums[curLevel];
@@ -31,24 +28,21 @@ public class Subsets_II_90 {
             backTrack(nums, curLevel + 1, new ArrayList<>(cur), res);
             // 放入当前列表
             addValue(cur, value);
-            //cur.add(value);
+//            cur.add(value);
             backTrack(nums, curLevel + 1, new ArrayList<>(cur), res);
         }
     }
 
+    // 如果非要进行优化的话，该部分还可以用二分查找
     public static void addValue(List<Integer> cur, Integer value) {
-        if (cur.size() == 0) {
-            cur.add(value);
-            return;
-        }
         for (int i = 0; i < cur.size(); i++) {
-            if (value >= cur.get(i)) {
-                cur.add(i + 1, value);
-            } else {
-                cur.add(i - 1, value);
+            if (value <= cur.get(i)) {
+                cur.add(i, value);
+                return;
             }
         }
-
+        // 列表为空/当前元素比列表中的所有元素都大
+        cur.add(value);
     }
 
     public static void main(String[] args) {
