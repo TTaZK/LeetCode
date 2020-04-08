01af8bff6f1235b7eaa1ab99542610e4ad26e216
张凯凯 <zhangkaikai@bytedance.com>
2020-04-08 11:56:41 +0800

    add no.347

---
+++ src/leetcode_with_name/Top_K_Frequent_Elements_347.java
@@ -0,0 +1,28 @@
+package leetcode_with_name;
+
+import java.util.*;
+
+public class Top_K_Frequent_Elements_347 {
+    // 一般 topK 首先想到的是堆
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap =
+                new PriorityQueue<>((a, b) -> (b.getValue() - a.getValue()));
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            maxHeap.add(entry);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        while (res.size() < k) {
+            Map.Entry<Integer, Integer> entry = maxHeap.poll();
+            res.add(entry.getKey());
+        }
+        return res;
+
+    }
+
+}
